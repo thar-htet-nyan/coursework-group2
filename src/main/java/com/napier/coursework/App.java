4677fabff834b92a14bdea488dfdd38e44dc5524
thar-htet-nyan <40614795@live.napier.ac.uk>
2023-07-06 14:42:43 +0630

    added docker composer

--- src/main/java/com/napier/coursework/App.java
+++ src/main/java/com/napier/coursework/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args){
 
         // print hello world
-        System.out.println("Hello World!");
+        System.out.printf("Hello World!");
 
     }
 
